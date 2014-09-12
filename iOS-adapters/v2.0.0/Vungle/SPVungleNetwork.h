ae0b3a1aa4883cfc65f131cf2213caaf1ff1f94f
Paweł Kowalczyk <pawel.kowalczyk@codete.com>
2014-09-12 14:13:55 +0200

    Updated Vungle iOS adapter to 2.1.0 (Vungle SDK 3.0.9)

--- iOS-adapters/v2.0.0/Vungle/SPVungleNetwork.h
+++ iOS-adapters/v2.0.0/Vungle/SPVungleNetwork.h
@@ -1,13 +1,20 @@
 //
-//  SponsorPay iOS SDK - Vungle Adapter v.2.0.0
+//  SponsorPay iOS SDK - Vungle Adapter v.2.1.0
 //
 //  Created by Daniel Barden on 13/01/14.
 //  Copyright (c) 2014 SponsorPay. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
+#import <VungleSDK/VungleSDK.h>
 #import "SPBaseNetwork.h"
 
 @interface SPVungleNetwork : SPBaseNetwork
+
+@property (nonatomic, copy) NSString *appId;
+@property (nonatomic, copy) NSString *orientation;
+@property (nonatomic, readonly) UIInterfaceOrientationMask orientationMask;
+@property (nonatomic, copy) NSNumber *showClose;
 
 @end
