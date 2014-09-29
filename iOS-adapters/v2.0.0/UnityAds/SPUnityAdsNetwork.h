d8f996a6b8daf6669181e7b842d35e1c739e32a8
Paweł Kowalczyk <pawel.kowalczyk@codete.com>
2014-09-29 13:19:34 +0200

    Transition from Applifier to UnityAds adapter 2.1.0 (UnityAds 1.3.8)

---
+++ iOS-adapters/v2.0.0/UnityAds/SPUnityAdsNetwork.h
@@ -0,0 +1,18 @@
+//
+//  SponsorPay iOS SDK - UnityAds Adapter v.2.1.0
+//
+//  Created on 13/01/14.
+//  Copyright (c) 2014 Fyber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SPBaseNetwork.h"
+
+@class SPUnityAdsNetwork;
+@compatibility_alias SPApplifierNetwork SPUnityAdsNetwork;
+
+//#define UNITY_ADS_TEST_MODE
+
+@interface SPUnityAdsNetwork : SPBaseNetwork
+
+@end
---
+++ iOS-adapters/v2.0.0/UnityAds/SPUnityAdsRewardedVideoAdapter.h
@@ -0,0 +1,24 @@
+//
+//  SPUnityAdsAdapter.h
+//  SponsorPaySample
+//
+//  Created on 10/1/13.
+//  Copyright (c) 2013 Fyber. All rights reserved.
+//
+
+@class SPUnityAdsRewardedVideoAdapter;
+@compatibility_alias SPApplifierRewardedVideoAdapter SPUnityAdsRewardedVideoAdapter;
+
+#import <Foundation/Foundation.h>
+#import "SPRewardedVideoNetworkAdapter.h"
+
+
+#import <UnityAds/UnityAds.h>
+
+@class SPUnityAdsNetwork;
+
+@interface SPUnityAdsRewardedVideoAdapter : NSObject<SPRewardedVideoNetworkAdapter, UnityAdsDelegate>
+
+@property (nonatomic, weak) SPUnityAdsNetwork *network;
+
+@end
