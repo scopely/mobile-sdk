5be879097f5ba3eae81ab88dadc023d47107baa4
Piotr Ruchwa <piotr.ruchwa@codete.com>
2014-11-03 10:28:44 +0100

    UnityAds adapter version 2.2.0 (UnityAds SDK 1.3.8)

--- iOS-adapters/v2.0.0/UnityAds/SPUnityAdsNetwork.h
+++ iOS-adapters/v2.0.0/UnityAds/SPUnityAdsNetwork.h
@@ -1,5 +1,6 @@
 //
-//  SponsorPay iOS SDK - UnityAds Adapter v.2.1.0
+//  SPUnityAdsNetwork.m
+//  Fyber iOS SDK - UnityAds Adapter v.2.2.0
 //
 //  Created on 13/01/14.
 //  Copyright (c) 2014 Fyber. All rights reserved.
@@ -7,6 +8,17 @@
 
 #import <Foundation/Foundation.h>
 #import "SPBaseNetwork.h"
+
+/**
+ Network class in charge of integrating Unity Ads library
+ 
+ ## Version compatibility
+ 
+ - Adapter version: 2.2.0
+ - Fyber SDK version: 6.5.2
+ - Unity Ads SDK version: 1.3.8
+ 
+ */
 
 @class SPUnityAdsNetwork;
 @compatibility_alias SPApplifierNetwork SPUnityAdsNetwork;
